c747e1f51fef32b377c6c7714d637059e7e1fd51
ArhaamAcademy <35983296+ArhaamAcademy@users.noreply.github.com>
2018-12-23 14:48:36 +0600

    Update CRUD-in-ReadBean.php

--- CRUD-in-ReadBean.php
+++ CRUD-in-ReadBean.php
@@ -5,6 +5,11 @@ CRUD in ReadBean
 
 //READ
 
+//Select with multiple conditions
+
+$findall = R::getAll('select * from pagecontent where page_id= :page_id AND status = :status', 
+array(':page_id'=>$page_id,':status' => $status));
+
 //UPDATE
 
 //DELETE
