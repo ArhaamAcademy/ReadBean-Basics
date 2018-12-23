f9e788693fecb7a7e13701e44515c22120e51b88
ArhaamAcademy <35983296+ArhaamAcademy@users.noreply.github.com>
2018-12-23 17:54:50 +0600

    Update CRUD-in-ReadBean.php

--- CRUD-in-ReadBean.php
+++ CRUD-in-ReadBean.php
@@ -10,6 +10,13 @@ CRUD in ReadBean
 $findall = R::getAll('select * from pagecontent where page_id= :page_id AND status = :status', 
 array(':page_id'=>$page_id,':status' => $status));
 
+$beans=R::find("books","active=? AND id=?",array(1,1));
+
+$bean=R::findOne("books","active=? AND id=?",array(1,1));
+
+$needles = R::find('needle',' haystack = :haystack ORDER BY :sortorder', 
+    array( ':sortorder'=>$sortorder, ':haystack'=>$haystack ));
+
 //UPDATE
 
 //DELETE
